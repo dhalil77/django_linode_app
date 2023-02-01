6bebdc678218d616a03558ed91c5e46e2fb14652
KAMDEM TCHIBONSOU DHALIL (External) <DHALIL.KAMDEMTCHIBONSOU.Guest@saipem.com>
2023-02-01 16:45:19 +0100

    new release

--- app/models.py
+++ app/models.py
@@ -2,17 +2,18 @@ from django.db import models
 
 # Create your models here.
 
-
 class VoteUser(models.Model):
     # id = models.IntegerField(primary_key = True)
-    id = models.AutoField(primary_key = True, unique=True)
+    User_id = models.AutoField(primary_key = True, unique=True)
     first_name = models.CharField(max_length=30 , unique=True )
     last_name = models.CharField(max_length=30, unique=True)
-    # email = models.EmailField('User Email', unique=True)
     telephone = models.CharField(max_length=30, unique=True)
     choix = models.CharField(max_length=50)
-    # updated_at = models.DateTimeField('Last Updated')
+    ville = models.CharField(max_length=50)
+    adresse = models.CharField(max_length=250)
+    status = models.BooleanField(default=False)
     updated_at = models.DateTimeField(auto_now_add=True)
+
 
     def __str__(self):
         return self.first_name + " " + self.last_name
